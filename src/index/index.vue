84754328765ff1ca02b5282fedb254808b7df805
zhangxianhong <zhangxianhong@forensix.cn>
2023-05-31 09:55:54 +0800

    feat: '添加基础配置'

---
+++ src/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+  <div>
+    <div>
+      一个基于VUE+VITE+ELECTRON+TS的脚手架模板
+    </div>
+    <div style="margin-top: 16px;">
+      <button @click="openNewWin">新建窗口</button>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { useIpcRenderer } from "@vueuse/electron";
+const ipcRenderer = useIpcRenderer();
+const openNewWin = () => {
+  ipcRenderer.send("window-new", {
+    route: "/helloworld",
+    width: 500,
+    height: 500,
+  });
+};
+</script>
